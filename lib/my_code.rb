70209674b6fe257d3061bfb79ec4ea56ed27d510
Monsur Khan <monsur.khan@gmail.com>
2019-10-25 02:36:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,15 +1,9 @@
 # Your Code Here
 
 def reduce (array, starting_point = 0)
-  # if starting_point
-  #   starting_point = starting_point
-  # end
-  
-  sum = 0
-  
   array.length.times do |index|
-    sum += array[index]
+    starting_point += array[index]
   end
   
-  return sum + starting_point
+  starting_point
 end
