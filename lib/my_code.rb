4a9cc5581162c881519f14e4cd620d7791c4f7d9
Monsur Khan <monsur.khan@gmail.com>
2019-10-25 02:28:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,20 @@
 # Your Code Here
+
+def reduce (array, starting_point = 100)
+  sum = 0
+  if starting_point
+    starting_point = starting_point
+  else
+    starting_point = 100
+  end
+  
+  array.length.times do |index|
+    sum += yield(array[index], starting_point)
+  end
+  
+  return sum + starting_point
+end
+
+array = [1, 2, 3]
+
+reduce(array)
