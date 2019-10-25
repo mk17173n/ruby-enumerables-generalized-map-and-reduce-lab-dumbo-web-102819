615120279e270fe20de60da38498b274874b7787
Monsur Khan <monsur.khan@gmail.com>
2019-10-25 02:39:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,9 +1,8 @@
 # Your Code Here
 
 def reduce (array, starting_point = 0)
-  yield(array.length.times do |index|
+  array.length.times do |index|
     starting_point += array[index]
-  end)
-  
-  starting_point
+  end
+  yield(starting_point)
 end
