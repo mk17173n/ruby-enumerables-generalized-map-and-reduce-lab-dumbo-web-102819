be9cdaca6537e31bf61c97bde741e2070da3f3c4
Monsur Khan <monsur.khan@gmail.com>
2019-10-25 02:41:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,7 +2,7 @@
 
 def reduce (array, starting_point = 0)
   array.length.times do |index|
-    yield(starting_point += array[index])
+    starting_point += array[index]
   end
   starting_point
 end
